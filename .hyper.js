b9050e59ba013dbce6603b85ce752c1d4f77a543
Carlos Azaustre <cazaustre@gmail.com>
2020-11-19 10:20:13 +0100

    Update hyper theme to match monokai-pro

--- .hyper.js
+++ .hyper.js
@@ -6,89 +6,90 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'stable',
+    updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: '"Operator Mono Lig", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: "Cascadia Code",
+    fontSmoothing: "antialiased",
 
     // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
+    fontWeight: "normal",
 
     // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
+    fontWeightBold: "bold",
 
     // line height as a relative unit
-    lineHeight: 1,
+    lineHeight: 1.15,
 
     // letter spacing as a relative unit
     letterSpacing: 0,
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: "#FA6378",
 
     // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
+    cursorAccentColor: "#000",
 
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'BLOCK',
+    cursorShape: "BLOCK",
 
     // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: false,
+    cursorBlink: true,
 
     // color of the text
-    foregroundColor: '#DEDEDF',
+    foregroundColor: "#EAF2F1",
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: 'rgba(29, 30, 32, 0.8)',
+    backgroundColor: "#1E1F2B",
 
     // terminal selection color
-    selectionColor: 'rgba(255, 110, 180, 0.3)',
+    selectionColor: "rgba(255, 110, 180, 0.3)",
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: "#161821",
 
     // custom CSS to embed in the main window
-    css: '',
+    css: "",
 
     // custom CSS to embed in the terminal window
-    termCSS: '',
+    termCSS: "",
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: "",
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: "",
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: "12px 14px",
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#FF7869',
-      green: '#4FCD8C',
-      yellow: '#FFFF80',
-      blue: '#0A2FC4',
-      magenta: '#FF6EB4',
-      cyan: '#80FFEA',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
+      black: "#000000",
+      red: "#FA6378",
+      green: "#BFD55A",
+      yellow: "#EEB300",
+      blue: "#0A2FC4",
+      magenta: "#C09BCA",
+      cyan: "#0FE2FF",
+      white: "#C7C7C7",
+      lightBlack: "#686868",
+      lightRed: "#FA6378",
+      lightGreen: "#BFD55A",
+      lightYellow: "#C5C79B",
+      lightBlue: "#6A76FB",
+      lightMagenta: "#C09BCA",
+      lightCyan: "#0FE2FF",
+      lightWhite: "#FFFFFF",
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -103,17 +104,17 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: "",
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
-    shellArgs: ['--login'],
+    shellArgs: ["--login"],
 
     // for environment variables
     env: {},
 
     // set to `false` for no bell
-    bell: 'SOUND',
+    bell: "SOUND",
 
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: false,
@@ -128,7 +129,7 @@ module.exports = {
     // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
     // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
     // (inside tmux or vim with mouse mode enabled for example).
-    macOptionSelectionMode: 'vertical',
+    macOptionSelectionMode: "vertical",
 
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
@@ -147,12 +148,21 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-],
+    // "hyper-relaxed",
+    // "hyper-flat"
+    // "hyperterm-duotone-darkspace"
+    // "hyper-rose-pine",
+    // "hyper-named-css-colors",
+    // "hyperocean",
+    // "hyper-relaxed",
+    // "hyper-website-theme"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: [
+    // "dracula-pro",
   ],
 
   keymaps: {
